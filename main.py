058a1f0ebe19a31eef44fbc082b98c4872d759e4
Thato <t.chitja369@gmail.com>
2023-12-02 21:54:42 +0200

    Extract first list

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 # Documentation
+import json
 import requests
 
-source = requests.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY')
-print(source.status_code)
-print("")
-print(source.json())
+#print values function
+#open dictionary function
+
+def myDictionaryFunction(extractedValue):
+        if isinstance(extractedValue, dict):
+            for key, value in extractedValue.items():
+                print(f"{key}")
+                myDictionaryFunction(value)
+        else:
+            print(extractedValue)
+
+def openDictionary():
+    print("Open Dictionary")
+
+def returnKeyValues():
+    print("Open Dictionary")
+
+# Retrieve the data from the source
+url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY'
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+    data = json.loads(response.text)
+    myDictionaryFunction(data)
+
+except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")
